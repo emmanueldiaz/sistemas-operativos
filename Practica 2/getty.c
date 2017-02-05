44438f9ed361bd5da40f72507441bde31b2c9a50
Emmanuel Diaz <emmanueldiaz@Emmanuels-MacBook-Pro.local>
2017-02-05 17:09:23 -0600

    added getty

--- Practica 2/getty.c
+++ Practica 2/getty.c
@@ -2,42 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define CHILDS 6
-
-void createChild();
-void childProcess(int processNum);
-
-int main()
+int main(int argc,char *argv[])
 {
-  pid_t pids[CHILDS], pid;
-
-  int status;
-  int finishedChilds = 0;
-  int i;
-
-  for (i = 0; i < CHILDS; i++)
-  {
-    if ((pids[i] = fork()) < 0)
-    {
-      printf("Failed to fork process %d\n", i + 1);
-    }
-    else if (pids[i] == 0)
-    {
-      childProcess(i);
-    }
-  }
-  printf("here end\n");
-  while (finishedChilds < 5)
-  {
-    pid = wait(&status);
-    printf("proceso %d termino con status %d\n", pid, status);
-    finishedChilds++;
-  }
-}
-
-void childProcess(int processNum)
-{
-  printf("proceso %d con pid %d\n", processNum, getpid());
-  sleep(rand() % 10);
-  exit(0);
+	int i;
+	printf("Hola mundo, mi pid es %d\n",getpid());
+	for(i=0;i<argc;i++)
+		printf("%s\n",argv[i]);
 }
---
+++ Practica 2/init.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define CHILDS 6
+
+void createChild();
+void childProcess(int processNum);
+
+int main()
+{
+  pid_t pids[CHILDS], pid;
+
+  int status;
+  int finishedChilds = 0;
+  int i;
+
+  for (i = 0; i < CHILDS; i++)
+  {
+    if ((pids[i] = fork()) < 0)
+    {
+      printf("No de pudo hacer fork del proceso %d\n", i + 1);
+    }
+    else if (pids[i] == 0)
+    {
+      childProcess(i);
+    }
+  }
+  while (finishedChilds < CHILDS)
+  {
+    pid = wait(&status);
+    printf("proceso %d termino con status %d\n", pid, status);
+    finishedChilds++;
+  }
+}
+
+void childProcess(int processNum)
+{
+  execlp("xterm", "-e", "./getty", NULL);
+  exit(0);
+}
