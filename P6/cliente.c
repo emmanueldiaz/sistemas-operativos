791c1b54fd934ba2442b6e8c2a792acd18ad4547
emmanueldiaz <rextype15@gmail.com>
2017-03-25 20:00:51 -0600

    update

--- P6/cliente.c
+++ P6/cliente.c
@@ -3,19 +3,19 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-FILE *fp;
-char coladest[10];
-// Valida que reciba el argumento
-if(argc!=2)
-{
-perror("Error en los argumentos\n");
-exit(EXIT_FAILURE);
-}
-sprintf(coladest,"fifo_%d",atoi(argv[1]));
-printf("Trabajo a enviarse en %s\n",coladest);
-// Abre el pipe del servidor 1 en modo de escritura
-fp=fopen(coladest,"w+");
-// Escribe en el pipe, en este caso el pid del proceso
-fprintf(fp,"%d",getpid());
-fclose(fp);
+	FILE *fp;
+	char coladest[10];
+	// Valida que reciba el argumento
+	if(argc!=2)
+	{
+		perror("Error en los argumentos\n");
+		exit(EXIT_FAILURE);
+	}
+	sprintf(coladest,"fifo_%d",atoi(argv[1]));
+	printf("Trabajo a enviarse en %s\n",coladest);
+	// Abre el pipe del servidor 1 en modo de escritura
+	fp=fopen(coladest,"w+");
+	// Escribe en el pipe, en este caso el pid del proceso
+	fprintf(fp,"%d",getpid());
+	fclose(fp);
 }
--- P6/servers.c
+++ P6/servers.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/sem.h>
+
+
 #define MAXSERVERS 3
+
+int semaforo[MAXSERVERS];
+
 int main()
 {
-int i;
-int pid;
-int status;
-char numserver[10];
-for(i=0;i<MAXSERVERS;i++)
-{
-pid=fork();
-if(pid==0)
-{
-sprintf(numserver,"%d",i);
-execlp("xterm", "xterm", "-e", "./servermsg",
-numserver,NULL);
-}
-}
-for(i=0;i<MAXSERVERS;i++)
-wait(&status);
+	int i;
+	int pid;
+	int status;
+	char numserver[10];
+	
+	for(i=0;i<MAXSERVERS;i++)
+	{
+		semaforo[i]=semget((key_t)0x1234,1,0666|IPC_CREAT); // Pedir al SO que me de un semáforo
+		semctl(semaforo[i],0,SETVAL,1);	// Inicializa el elemento 0 del arreglo de semáforos con 1
+
+		pid=fork();
+		if(pid==0)
+		{
+			sprintf(numserver,"%d",i);
+			execlp("xterm", "xterm", "-e", "./servermsg",
+			numserver,NULL);
+		}
+	}
+	for(i=0;i<MAXSERVERS;i++)
+		wait(&status);
 }
